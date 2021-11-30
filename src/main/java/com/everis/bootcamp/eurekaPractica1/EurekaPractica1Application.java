df00901e64c8258dd4f5655cf5b47850194d2449
Francisco <fran89rbb@hotmail.com>
2021-11-30 10:23:26 +0100

    first commit

---
+++ src/main/java/com/everis/bootcamp/eurekaPractica1/EurekaPractica1Application.java
@@ -0,0 +1,15 @@
+package com.everis.bootcamp.eurekaPractica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaPractica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaPractica1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/bootcamp/eurekaPractica1/EurekaPractica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.bootcamp.eurekaPractica1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaPractica1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
